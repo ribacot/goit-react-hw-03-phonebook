8491b2cacdb2e24088a995810ea11f7b0296a65a
ribacot <remontdiskov1979@gmail.com>
2023-08-01 23:41:41 +0300

    finsh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,17 +10,20 @@ const INITIAL_STATE = {
   contacts: [],
   filter: '',
 };
-
+const contactsLs = "contacts";
 export class App extends Component {
   state = {
     ...INITIAL_STATE,
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(contactsLs)) || [];
+this.setState({contacts})
+
+  }
+  componentDidUpdate() {
+    localStorage.setItem(contactsLs, JSON.stringify(this.state.contacts));
+  }
 
   hendleSubmit = ({ name, number }) => {
     this.setState(precState => {
@@ -60,6 +63,7 @@ export class App extends Component {
   };
 
   render() {
+    console.log('re-render')
     return (
       <>
         <Container title="Phone book">
